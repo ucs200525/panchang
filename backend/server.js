7b250bc837d8e712f842281ef0c930915404b72f
ucs200525 <chandrasekharupadhyayula@gmail.com>
2024-12-01 10:34:23 +0530

    completed v1

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const cors = require('cors'); // Import the cors package
-const morgan = require('morgan'); // Import the morgan package
-const logger = require('./utils/logger'); // Import the logger
+const cors = require('cors');
+const morgan = require('morgan');
+const logger = require('./utils/logger');
 const dotenv = require('dotenv');
 const app = express();
 const panchangRoutes = require('./routes/panchangRoutes');
@@ -9,19 +9,21 @@ dotenv.config();
 
 app.use(express.json());
 const port = process.env.PORT || 4000;
-const geoUsername = process.env.GEO_USERNAME || ucs05;
+
 // CORS Configuration
 app.use(cors({
-    origin: '*', // Allow requests from any origin
+    origin: '*', // Allow all origins (can be restricted for production)
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Specify allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
 }));
 
-app.use('/api', panchangRoutes);     
 // Logger Middleware
-app.use(morgan('combined')); // Use 'combined' for detailed logs
+app.use(morgan('combined'));
 
+// Routes
+app.use('/api', panchangRoutes);
 
+// Start Server
 app.listen(port, () => {
     logger.info(`Server is running on port ${port}`);
 });
